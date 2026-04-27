637b4bc7ce7670d2aa61fcd2d5ed83d6a60ec114
JoseFelisbino <lu31660@gmail.com>
2026-04-27 19:12:56 -0300

    adicionando produto no carrinho

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 function adicionar() {
     let produto = document.getElementById("produto").value;
+    let quantidade = document.getElementById("quantidade").value;
+
     let nomeProduto = produto.split("-")[0];
     let valorUnitario = produto.split("R$")[1];
-    let quantidade = document.getElementById("quantidade");
-
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
 
     let preco = quantidade.value * valorUnitario;
-    alert(preco);
+
+    let carrinho = document.getElementById("lista-produtos");
+
+    carrinho.innerHTML =  carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
+        <section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x </span>${nomeProduto}<span class="texto-azul">${valorUnitario}</span>
+        </section>
+      </section>`
+
 
 }
 
